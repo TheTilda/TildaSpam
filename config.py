281a5b365548568b3b16335a1114fc911b71735d
TheTilda <67949697+TheTilda@users.noreply.github.com>
2022-02-18 22:35:20 +0700

    сменил на время все id

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = 123456 #Ваш api_id
-api_hash = 'Ваш api_hash'
-token = 'Токен бота'
-admin = 1234567 #id админа
+api_id = 14466588
+api_hash = '342a1b1f91d158e5d33ddf00d76ebf4e'
+token = '5273176481:AAGqLhTwkQPMT8_Hrt8t6xyl6Bv_BbiNDJw'
+admin = 1117551115
